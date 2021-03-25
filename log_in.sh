c59435d66fa4727f54268cc63ebb67714f63b399
simiaolin <simiao.lin@student.kuleuven.be>
2021-03-25 23:04:33 +0100

    add log in shell

---
+++ log_in.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -v
+eval `ssh-agent -s` && ssh-add ~/.ssh/id_rsa
+scp -r /Users/ary/CLionProjects/bdap_2/  r0829520@balen.cs.kotnet.kuleuven.be:/home/r0829520/assign2
+ssh r0829520@balen.cs.kotnet.kuleuven.be
+cd /home/r0829520/assign2/bdap_2
+./install.sh
