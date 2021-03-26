4b560d282d54b648d466c3350701b6064169e820
simiaolin <simiao.lin@student.kuleuven.be>
2021-03-26 08:26:19 +0100

    refine the shell file

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 source ~/.bash_profile
 rm -rf build
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr/local/bin/ ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/home/r0829520/assign2/ ..
 make -j10
 make install
-cd ../test/build
-cmake -DCMAKE_PREFIX_PATH:PATH=/usr/local/bin/ ..
+cd ../test
+rm -rf build
+mkdir build && cd build
+cmake -DCMAKE_PREFIX_PATH:PATH=/home/r0829520/assign2/ ..
 make -j10
--- log_in.sh
+++ log_in.sh
@@ -4,3 +4,4 @@ scp -r /Users/ary/CLionProjects/bdap_2/  r0829520@balen.cs.kotnet.kuleuven.be:/h
 ssh r0829520@balen.cs.kotnet.kuleuven.be
 cd /home/r0829520/assign2/bdap_2
 ./install.sh
+cd /home/r0829520/assign2/bdap_2/test/build
